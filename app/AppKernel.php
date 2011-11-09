3b428e34527c681fd3f37dfc0ad93c3e8502186c
Julien Kirsch <informatic.revolution@gmail.com>
2011-11-09 10:47:16 -0500

    update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),     
             new IrErp\DashboardBundle\IrErpDashboardBundle(),
+            new IrErp\UserBundle\IrErpUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
